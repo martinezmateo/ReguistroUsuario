ffe28920206747d151d9a89b331c7779395d6bf9
XvenusD <zxdmateozxd@gmail.com>
2023-10-08 15:09:33 -0500

    creacion de modelo user

---
+++ documentos/creandoconciencia/demo/demo/src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String apellido;
+
+}
